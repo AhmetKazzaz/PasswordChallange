da999cb8a93994b4746b32eab8cd448be7647bfb
Ahmed <ahmet.kazzaz@solidict.com>
2021-06-19 12:09:47 -0700

    Wrote Function to convert email to binary. Added kotlin extension plugin to acess views easier.

--- app/src/main/java/com/example/passwordchallange/MainActivity.kt
+++ app/src/main/java/com/example/passwordchallange/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.passwordchallange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun convertEmailToBinary(emailToConvert: String): String {
+        val bytesFromString = emailToConvert.toByteArray()
+        val result = StringBuilder()
+        for (byte in bytesFromString) {
+            var temp = byte.toInt()
+            (0..7).forEach { _ ->
+                result.append(if (temp and 128 == 0) 0 else 1)
+                temp = temp shl 1
+            }
+        }
+        return result.toString()
+    }
+
 }
